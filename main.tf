17e189e8d398e8332a2552184a98ec09b4194ca5
Radhika <radhika.b4329@gmail.com>
2023-05-20 19:10:22 +0530

    update vpc module

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "network" {
+module "vpc" {
   source               = "github.com/Radhikab4329/tf-module-vpc"
   env                  = var.env
   default_vpc_id       = var.default_vpc_id
@@ -8,7 +8,19 @@ module "network" {
 
 }
 
+module "subnets" {
+  source               = "github.com/Radhikab4329/tf-module-subnets"
+  env                  = var.env
+  default_vpc_id       = var.default_vpc_id
 
+
+  for_each             = var.subnets
+  cidr_block           = each.value.cidr_block
+  availability_zone    = each.value.availability_zone
+  name                 = each.value.name
+  vpc_id               = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+
+}
 
 
 
