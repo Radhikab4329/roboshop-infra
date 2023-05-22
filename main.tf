f2174a6afc63ce817914e759e572efb9ae044662
Radhika <radhika.b4329@gmail.com>
2023-05-22 21:57:29 +0530

    update module subnet

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
 
   for_each             = var.vpc
   cidr_block           = each.value.cidr_block
-#  public_subnet_ids    = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  public_subnet_ids    = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
 
 }
 
