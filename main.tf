052badbd7398f029457d4deec0e6337266602290
Radhika <radhika.b4329@gmail.com>
2023-05-23 13:11:29 +0530

    update module subnet

--- main.tf
+++ main.tf
@@ -5,24 +5,25 @@ module "vpc" {
 
   for_each             = var.vpc
   cidr_block           = each.value.cidr_block
+  subnets              = each.value.subnets
 }
 
-module "subnets" {
-  source               = "github.com/Radhikab4329/tf-module-subnets"
-  env                  = var.env
-  default_vpc_id       = var.default_vpc_id
-
-
-  for_each                  = var.subnets
-  cidr_block                = each.value.cidr_block
-  availability_zone         = each.value.availability_zone
-  name                      = each.value.name
-  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw               = lookup(each.value, "internet_gw", false)
-  nat_gw                    = lookup(each.value, "nat_gw", false)
-
-}
+#module "subnets" {
+#  source               = "github.com/Radhikab4329/tf-module-subnets"
+#  env                  = var.env
+#  default_vpc_id       = var.default_vpc_id
+#
+#
+#  for_each                  = var.subnets
+#  cidr_block                = each.value.cidr_block
+#  availability_zone         = each.value.availability_zone
+#  name                      = each.value.name
+#  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+#  internet_gw               = lookup(each.value, "internet_gw", false)
+#  nat_gw                    = lookup(each.value, "nat_gw", false)
+#
+#}
 
 #output "subnet_ids" {
 #  value = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
